93b14679b38b3df3ad3a51ffcec2f8e5480369cd
gaazoo <gaazoo@yandex.ru>
2023-07-25 23:00:58 +0700

    Домашка готова

---
+++ task50/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Зададим размер исходного массива.");
+Console.Write("Введите количество строк в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию искомого элемента");
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+Random random = new Random();
+int[,] array = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.Write("|");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.Next(10, 99);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.Write("|");
+    Console.WriteLine();
+}
+if (pos1 < 0 || pos1 > array.GetLength(0) - 1 || pos2 < 0 || pos2 > array.GetLength(1) - 1)
+{
+    Console.WriteLine("Искомый элемент не существует.");
+}
+else
+{
+    Console.WriteLine($"Значение элемента массива = {array[pos1, pos2]}");
+}
+Console.WriteLine();
---
+++ task52/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Random random = new Random();
+int[,] array = new int[random.Next(1, 10), random.Next(1, 10)];
+Console.WriteLine("Сгенерированный массив:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.Write("|");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.Next(1, 10);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.Write("|");
+    Console.WriteLine();
+}
+Console.WriteLine($"Количество элеметов в каждом столбце: {array.GetLength(0)}");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine($"Среднее арифметическое в {j + 1} столбце = {Math.Round(sum / array.GetLength(0), 3)} ");
+}
+Console.WriteLine();
